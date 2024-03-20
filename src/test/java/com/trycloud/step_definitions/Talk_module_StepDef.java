30af2166bfb4ea06011c1a568f09ca967970e7c1
ishanovamaya@gmail.com <ishanovamaya@gmail.com>
2024-03-20 10:30:05 -0400

    Updated talkModule feature and talkModule_StepDef

---
+++ src/test/java/com/trycloud/step_definitions/Talk_module_StepDef.java
@@ -0,0 +1,88 @@
+package com.trycloud.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Talk_module_StepDef {
+
+    @Given("user logged in and in the dashboard page")
+    public void user_logged_in_and_in_the_dashboard_page() {
+
+    }
+    @When("user navigates and clicks on Talk Module icon")
+    public void user_navigates_and_clicks_on_talk_module_icon() {
+
+    }
+    @Then("talk module page opens")
+    public void talk_module_page_opens() {
+
+    }
+    @When("user inters a participant and clicks the add button")
+    public void user_inters_a_participant_and_clicks_the_add_button() {
+
+    }
+    @Then("conversation content pops-up with searched participant's name on it")
+    public void conversation_content_pops_up_with_searched_participant_s_name_on_it() {
+
+    }
+    @Then("user checks a checkbox to allow guests to join")
+    public void user_checks_a_checkbox_to_allow_guests_to_join() {
+
+    }
+    @Then("user clicks to the Add participants button")
+    public void user_clicks_to_the_add_participants_button() {
+
+    }
+    @Then("all participants list pups-up")
+    public void all_participants_list_pups_up() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user selects a participant from the list and clicks into Create conversation button")
+    public void user_selects_a_participant_from_the_list_and_clicks_into_create_conversation_button() {
+
+    }
+    @Then("copy the conversation link option pops-up")
+    public void copy_the_conversation_link_option_pops_up() {
+
+    }
+    @Then("user clicks into Close button")
+    public void user_clicks_into_close_button() {
+
+    }
+    @Then("user can see newly created conversation on the list")
+    public void user_can_see_newly_created_conversation_on_the_list() {
+
+    }
+
+
+
+
+
+
+
+    @Given("user navigates to newly created conversation and clicks meatball button")
+    public void userNavigatesToNewlyCreatedConversationAndClicksMeatballButton() {
+    }
+
+    @Then("list of options drops down")
+    public void listOfOptionsDropsDown() {
+    }
+
+    @Then("the Delete conversation option is visible")
+    public void theDeleteConversationOptionIsVisible() {
+    }
+
+    @Then("user clicks to delete and verification of deletion is pops-up")
+    public void userClicksToDeleteAndVerificationOfDeletionIsPopsUp() {
+    }
+
+    @Then("user clicks Yes button")
+    public void userClicksYesButton() {
+    }
+
+    @Then("verify if conversation had been deleted")
+    public void verifyIfConversationHadBeenDeleted() {
+    }
+}
--- src/test/resources/features/talk_module_maya.feature
+++ src/test/resources/features/talk_module_maya.feature
@@ -3,17 +3,14 @@ Feature: Talk Module Functionality (Automation)
   Agile story:
   As a user, I should be able to create a new conversation, add participant under the Talk module
 
-  Scenario: User already logged in
-    Given user logged in
-    Then user is on the Dashboard page
-
   Scenario: User can create a new conversation by adding any selected participant
-    Given user navigates and clicks on Talk Module icon
+    Given user logged in and in the dashboard page
+    When user navigates and clicks on Talk Module icon
     Then talk module page opens
-    When user inters a conversationer and clicks the add button
-    Then conversetion content pops-up with searched conversationer's name on it
+    When user inters a participant and clicks the add button
+    Then conversation content pops-up with searched participant's name on it
     Then user checks a checkbox to allow guests to join
-    Then user clickes to the Add participants button
+    Then user clicks to the Add participants button
     Then all participants list pups-up
     Then user selects a participant from the list and clicks into Create conversation button
     Then copy the conversation link option pops-up
@@ -21,14 +18,10 @@ Feature: Talk Module Functionality (Automation)
     Then user can see newly created conversation on the list
 
 
-
-
-
-
   Scenario: User can delete a conversation
-    Given user navigates to newly created conversation and clicks meetball button
+    Given user navigates to newly created conversation and clicks meatball button
     Then list of options drops down
-    Then the Delite conversation option is visible
+    Then the Delete conversation option is visible
     Then user clicks to delete and verification of deletion is pops-up
     Then user clicks Yes button
-    Then verify if conversation had been delited
+    Then verify if conversation had been deleted
